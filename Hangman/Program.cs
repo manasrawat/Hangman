968baa27b9b52ca6dd162419a73e8b4bc7264598
Manas Rawat <manas.rawat2002@gmail.com>
2017-04-08 11:50:17 +0100

    Update #1

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -8,60 +8,63 @@ using static System.Console;
 
 namespace Hangman {
     class Program {
-        static int[] xPos = { 5, 4, 6, 5, 4, 6 }, yPos = { 2, 3, 3, 4, 5, 5 };
+        static int lives, lettersFound;
+        static int[] xPos = { 6, 4, 6, 4, 5, 5, 5 }, yPos = { 5, 5, 3, 3, 4, 3, 2 };
         static List<char> taken = new List<char>();
         static Random random = new Random();
         static void Main(string[] args) {
+            Title = "Hangman";
             bool cont = true;
             do {
-                int lives = 5, lettersFound = 0;
+                lives = 6; lettersFound = 0;
                 Clear();
                 taken.Clear();
                 string hangman = " ----¬";
-                string[] parts = { " |", " O", "- -", " |", "/ \\" };
-                foreach (string part in parts) hangman += "\n |  " + part;
+                string[] parts = { "\\", "/", "-", "-", "|", "|", "O" };
+                for (int i = 0; i < 5; i++) hangman += "\n |" + ((i == 0) ? "   |": null);
                 hangman += "\n |\n---";
-                ForegroundColor = ConsoleColor.Green; WriteLine(hangman);
+                ForegroundColor = ConsoleColor.DarkGreen; WriteLine(hangman);
                 IEnumerable<string> dictionary = File.ReadLines("Dictionary.txt");
                 int line = random.Next(0, dictionary.Count());
                 string word = dictionary.ElementAt(line), dashes = "";
                 foreach (char c in word) dashes += "-";
-                ForegroundColor = ConsoleColor.DarkMagenta; WriteLine(dashes + "\n" + word);
+                ForegroundColor = ConsoleColor.DarkMagenta; WriteLine(dashes);
                 while (lives >= 0 && lettersFound < word.Length) {
                     string letterstr = ReadLine();
-                    while (!Regex.IsMatch(letterstr, @"^[A-Za-z]$")) {
-                        CursorTop = 10; ForegroundColor = ConsoleColor.DarkRed; WriteLine("Invalid input");
-                        Thread.Sleep(1250);
-                        CursorTop = 10; ForegroundColor = ConsoleColor.DarkMagenta; WriteLine("             ");
-                        CursorTop = 10; letterstr = ReadLine();
+                    bool addChar = true, deduct = true;
+                    if (dictionary.Any(str => str == letterstr)) {
+                        if (letterstr == word) lettersFound = word.Length;
                     }
-                    char letter = Convert.ToChar(letterstr);
-                    bool addChar = true, charPresent = false;
-                    foreach (char c in taken) if (letter == c) addChar = false;
-                    int currentY = CursorTop;
-                    for (int i = 0; i < word.Length; i++) {
-                        if (letter == word[i]) {
-                            charPresent = true;
-                            if (addChar) {
-                                taken.Add(letter);
-                                SetCursorPosition(i, 8); WriteLine(word[i]);
-                                lettersFound += 1;
+                    else if (Regex.IsMatch(letterstr, @"^[A-Za-z]$")) {
+                        char letter = Convert.ToChar(letterstr);
+                        foreach (char c in taken) if (letter == c) addChar = false;
+                        int currentY = CursorTop;
+                        for (int i = 0; i < word.Length; i++) {
+                            if (letter == word[i]) {
+                                deduct = false;
+                                if (addChar) {
+                                    taken.Add(letter);
+                                    SetCursorPosition(i, 8); WriteLine(word[i]);
+                                    lettersFound += 1;
+                                }
                             }
                         }
+                    } else {
+                        deduct = false;
+                        CursorTop = 9; WriteLine(new string(' ', WindowWidth));
+                        CursorTop = 9; ForegroundColor = ConsoleColor.DarkRed; WriteLine("Invalid input");
+                        Thread.Sleep(1225);
+                        CursorTop = 9; WriteLine("             ");
+                        CursorTop = 9; ForegroundColor = ConsoleColor.DarkMagenta;
                     }
-                    if (!charPresent) {
-                        SetCursorPosition(xPos[lives], yPos[lives]); WriteLine(" ");
+                    if (deduct) {
+                        ForegroundColor = ConsoleColor.Green; SetCursorPosition(xPos[lives], yPos[lives]); WriteLine(parts[lives]);
                         lives -= 1;
                     }
-                    CursorTop = 10; WriteLine(" "); CursorTop = 10;
+                    ForegroundColor = ConsoleColor.DarkMagenta; CursorTop = 9; WriteLine(new string(' ', WindowWidth)); CursorTop = 9;
                 }
                 ForegroundColor = ConsoleColor.DarkCyan;
-                if (lettersFound == word.Length) {
-                    WriteLine("Congratulations! You survived.");
-                }
-                else {
-                    WriteLine("Unfortunately... you died.");
-                }
+                WriteLine((lettersFound == word.Length) ? "Congratulations! You survived." : "You died. The word was " + word);
                 ForegroundColor = ConsoleColor.DarkYellow; WriteLine("Wanna play again? Type 'no' to exit.");
                 string answer = ReadLine();
                 if (answer == "no") cont = false;
